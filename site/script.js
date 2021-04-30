db5a8b7fb4662c8c53c71bd400007c8c0e6a19a7
jaimesanchezgalvis <jaimesanchezgalvis@gmail.com>
2021-04-30 15:21:48 +0200

    second Cmmit

--- site/script.js
+++ site/script.js
@@ -1,42 +1,98 @@
-// get data by fetch
-function getdata() {
-    fetch("./data.json")
-        .then(res => res.json())
-        .then(data => {
-            createTable(data)
-        });
-    hideButton();
+let data = new Map();
+
+// connect stomp
+var url = "ws://localhost:8011/stomp";
+var client = Stomp.client(url);
+
+// called back after the client is connected and authenticated to the STOMP server
+var connect_callback = function () {
+    let createMapAndRender = function (message) {
+        if (message.body) {
+            let body = JSON.parse(message.body);
+            let midPrice = (body.bestBid + body.bestAsk) / 2;
+            let previusData = data.get(body.name);
+            data.set(body.name, {
+                ...body,
+                sparkline: previusData
+                    ? {
+                        x: [
+                            ...previusData.sparkline.x.filter(
+                                (item) => Date.now() - item < 30000),
+                            Date.now(),
+                        ],
+                        y: [
+                            ...previusData.sparkline.y.filter(
+                                (item, i) => Date.now() - previusData.sparkline.x[i] < 30000
+                            ),
+                            midPrice,
+                        ],
+                    }
+                    : { x: [Date.now()], y: [midPrice] },
+            });
+            createTable();
+        } else {
+            alert("got emty message");
+        }
+    }
+    client.subscribe("/fx/prices", createMapAndRender);
+
 };
-// hide button
-function hideButton() {
-    const button = document.querySelector(".btn");
-    button.style.display = "none";
-    createHome();
+
+// display the error´s message header
+var error_callback = function (error) {
+    console.log("error");
+    alert(error.headers.message);
+};
+var headers = {
+    login: "mylogin",
+    passcode: " mypasscode",
+    // additional header
+    "client-id": "my-client-id"
 }
-// create button home
-function createHome() {
-    let home = document.querySelector(".home");
-    if (home.style.display === "none") {
-        home.style.display = "block";
-    } else {
-        home.style.display = "none"
-    }
+// connection and authentication
+client.connect(headers, connect_callback)
+
+
+// createting table rows
+function createTableRow(value) {
+    const sparkElement = document.createElement("span");
+    const sparkline = new Sparkline(sparkElement);
+    sparkline.draw(value.sparkline.y);
+    let tr = document.createElement("tr");
+    let name = document.createElement("th");
+    name.textContent = value.name;
+    let bestBid = document.createElement("td");
+    bestBid.textContent = value.bestBid;
+    let bestAsk = document.createElement("td");
+    bestAsk.textContent = value.bestAsk;
+    let lastChangeBid = document.createElement("td");
+    lastChangeBid.textContent = value.lastChangeBid;
+    let lastChangeAsk = document.createElement("td");
+    lastChangeAsk.textContent = value.lastChangeAsk;
+    let trend = document.createElement("td");
+    trend.append(sparkElement);
+    tr.append(name, bestBid, bestAsk, lastChangeBid, lastChangeAsk, trend);
+    return tr;
 }
-// render data
-function createTable(data) {
-    // sort data
-    data.sort(function (a, b) { return a.bestBid - b.bestBid });
-    data.reverse();
-    // create dynamic table
-    for (let value of data) {
-        content.innerHTML += `
-        <tr>
-        <th>${value.name}</th>
-        <td>${value.bestBid}</td>
-        <td>${value.bestAsk}</td>
-        <td>${value.lastChangeBid}</td>
-        <td>${value.lastChangeAsk}</td>
-        </tr>
-        `
+
+
+
+function createTable() {
+    let valuearr = [];
+    let tbody = document.createElement("tbody");
+    tbody.id = "table-body";
+    // create an array from map 
+    for (let value of data.values()) {
+        valuearr.push(value);
     }
+    // sort array of objetct
+    valuearr.sort(function (a, b) {
+        return b.lastChangeBid - a.lastChangeBid;
+    });
+    valuearr.forEach((value) => {
+        tbody.appendChild(createTableRow(value));
+    });
+    // find tbody from html and repalce with new one 
+    let oldTbody = document.getElementById("table-body");
+    oldTbody.replaceWith(tbody);
 }
